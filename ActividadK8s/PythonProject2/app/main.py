f37bd6a96a7af06a8d94cb0a8f48b0d1e0676efb
alfonsogesto <127800118+alfonsogesto@users.noreply.github.com>
2026-02-11 20:08:15 -0300

    Add files via upload

---
+++ ActividadK8s/PythonProject2/app/main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+archivo = "data/notas.txt"
+
+@app.route('/')
+def home():
+    return "La API funciona correctamente (para la v2)"
+
+@app.route('/add/<nota>')
+def agregoNota(nota):
+    with open(archivo, "a") as f:
+        f.write(nota + "\n")
+    return nota
+
+@app.route('/list')
+def lista():
+    with open(archivo, "r") as f:
+        notas = f.read()
+    return notas
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
